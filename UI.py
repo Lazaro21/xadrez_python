2b3175eca8e6126c89aed99206b160ecccac192c
PinkPynchon <olsf21@gmail.com>
2021-03-19 17:00:12 -0300

    Adionando cor na impressão das pecas

--- UI.py
+++ UI.py
@@ -1,3 +1,18 @@
+from JogoDeTabuleiro.PecaXadrez import Cores
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def imprimir_tabuleiro(tabuleiro):
     for i in range(tabuleiro.linhas):
         print(8 - i, ": ", end='')
@@ -5,7 +20,10 @@ def imprimir_tabuleiro(tabuleiro):
             if tabuleiro[i][j] is None:
                 print(' - ', end='')
             else:
-                print(tabuleiro[i][j], end='')
+                if tabuleiro[i][j].COR == Cores.BRANCA:
+                    print(str(tabuleiro[i][j]), end='')
+                else:
+                    print(bcolors.OKBLUE + str(tabuleiro[i][j]) + bcolors.ENDC, end='')
         print("\n")
     print("     a  b  c  d  e  f  g  h")
 
