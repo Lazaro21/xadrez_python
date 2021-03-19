b3e4366448721f151bc267e88c005c3626f275bc
PinkPynchon <olsf21@gmail.com>
2021-03-19 16:32:42 -0300

    Criando excecão de tabuleiro

--- JogoDeTabuleiro/Tabuleiro.py
+++ JogoDeTabuleiro/Tabuleiro.py
@@ -11,9 +11,29 @@ class Tabuleiro(list):
 
     def peca(self, linha, coluna):
         linhas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        linhaNumero = linhas[linha].index()
-        return self.pecas[linhaNumero][coluna]
+        linha_numero = linhas[linha].index()
+
+        if not self.posicao_existe(linha, coluna):
+            raise TabuleiroException("Posicão digitada não existe. ")
+        return self.pecas[linha_numero][coluna]
 
     def colocar_peca(self, peca, posicao):
+        if self.ha_uma_peca_nessa_posicao(posicao.linha, posicao.coluna):
+            raise TabuleiroException("Posicão ocupada. ")
+
         self[posicao.linha][posicao.coluna] = peca
         peca.posicao = posicao
+
+    @staticmethod
+    def posicao_existe(linha, coluna):
+        return 0 <= linha <= 7 and 0 <= coluna <= 7
+
+    def ha_uma_peca_nessa_posicao(self, linha, coluna):
+        if self.posicao_existe(linha, coluna):
+            if self[linha][coluna] is None:
+                return True
+
+
+class TabuleiroException(Exception):
+    """Responsável por tratar excecões relacionadas ao jogo de xadrez"""
+    pass
